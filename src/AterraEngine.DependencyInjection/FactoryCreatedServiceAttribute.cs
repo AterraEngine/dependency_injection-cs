e53d04e753998e496c766bacf0b475cf8f52bb66
Anna Sas <directive.athena@gmail.com>
2024-11-27 16:00:47 +0100

    Feat: Project setup

---
+++ src/AterraEngine.DependencyInjection/FactoryCreatedServiceAttribute.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AterraEngine.DependencyInjection;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+public class FactoryCreatedServiceAttribute<TFactory, TService>(ServiceLifetime lifetime) : Attribute {
+    [UsedImplicitly] public ServiceLifetime Lifetime { get; } = lifetime;
+    [UsedImplicitly] public Type ServiceType { get; } = typeof(TService);
+    [UsedImplicitly] public Type FactoryType { get; } = typeof(TFactory);
+}
---
+++ src/AterraEngine.DependencyInjection/InjectableServiceAttribute.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AterraEngine.DependencyInjection;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+public class InjectableServiceAttribute<TService>(ServiceLifetime lifetime) : Attribute {
+    [UsedImplicitly] public ServiceLifetime Lifetime { get; } = lifetime;
+    [UsedImplicitly] public Type ServiceType { get; } = typeof(TService);
+}
