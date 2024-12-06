150828d5dc635a13b04fe587096043d7a9a62664
Anna Sas <anna.sas.dev@gmail.com>
2024-12-06 22:04:01 +0100

    Feat: Tools.AterraEngine.DependencyInjection

---
+++ tools/Tools.AterraEngine.DependencyInjection/Program.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CliArgsParser;
+using Microsoft.Extensions.DependencyInjection;
+using Tools.AterraEngine.DependencyInjection.Commands;
+
+namespace Tools.AterraEngine.DependencyInjection;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+internal static class Program {
+    public async static Task Main(string[] args) {
+        IServiceCollection serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddCliArgsParser(configuration =>
+            configuration
+                .SetConfig(new CliArgsParserConfig {
+                    Overridable = true,
+                    GenerateShortNames = true
+                })
+                .AddFromType<VersionBumpCommands>()
+        );
+
+        ServiceProvider provider = serviceCollection.BuildServiceProvider();
+
+        var argsParser = provider.GetRequiredService<IArgsParser>();
+        await argsParser.ParseAsyncLinear(args);
+    }
+}
