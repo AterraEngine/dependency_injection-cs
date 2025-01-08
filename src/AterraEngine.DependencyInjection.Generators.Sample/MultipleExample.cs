908489ee6ec4f57bdacb7e9677dc2061e53e20d4
Anna Sas <anna.sas.dev@gmail.com>
2025-01-08 15:51:50 +0100

    Feat: TUnit Update

--- src/AterraEngine.DependencyInjection.Generators.Sample/MultipleExample.cs
+++ src/AterraEngine.DependencyInjection.Generators.Sample/MultipleExample.cs
@@ -1,25 +1,16 @@
-﻿// // ---------------------------------------------------------------------------------------------------------------------
-// // Imports
-// // ---------------------------------------------------------------------------------------------------------------------
-// using Microsoft.Extensions.DependencyInjection;
-//
-// namespace AterraEngine.DependencyInjection.Generators.Sample;
-// // ---------------------------------------------------------------------------------------------------------------------
-// // Code
-// // ---------------------------------------------------------------------------------------------------------------------
-// // [InjectableService<ISecondDualExampleService>(ServiceLifetime.Singleton)]
-// [InjectableService<IFirstDualExampleService>(ServiceLifetime.Singleton)]
-// public class DualExampleService : IFirstDualExampleService, ISecondDualExampleService;
-//
-// public interface IFirstDualExampleService;
-//
-// public interface ISecondDualExampleService;
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.Extensions.DependencyInjection;
 
-namespace TestProject;
-        
-[AterraEngine.DependencyInjection.PooledInjectableService<IExamplePooled, ExamplePooled>]
-public class ExamplePooled : IExamplePooled {
-    public bool Reset() => true;
-}
-        
-public interface IExamplePooled : AterraEngine.DependencyInjection.IManualPoolable;
+namespace AterraEngine.DependencyInjection.Generators.Sample;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+// [InjectableService<ISecondDualExampleService>(ServiceLifetime.Singleton)]
+[InjectableService<IFirstDualExampleService>(ServiceLifetime.Singleton)]
+public class DualExampleService : IFirstDualExampleService, ISecondDualExampleService;
+
+public interface IFirstDualExampleService;
+
+public interface ISecondDualExampleService;
