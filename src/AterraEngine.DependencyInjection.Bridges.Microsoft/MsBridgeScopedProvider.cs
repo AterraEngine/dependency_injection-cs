d226f1d5b33bd83bed7bb58a2fcefafc5800872a
Anna Sas <anna.sas.dev@gmail.com>
2025-03-19 16:23:59 +0100

    Update MsBridgeScopedProvider.cs

--- src/AterraEngine.DependencyInjection.Bridges.Microsoft/MsBridgeScopedProvider.cs
+++ src/AterraEngine.DependencyInjection.Bridges.Microsoft/MsBridgeScopedProvider.cs
@@ -12,6 +12,9 @@ public class MsBridgeScopedProvider(ServiceContainer serviceContainer, IServiceP
         => base.GetService<TService>() ?? msServiceProvider.GetService<TService>();
 
     public override TService GetRequiredService<TService>() 
+        // yes I know this is not RequiredService, but the issue is that we need a way to check
+        //      if the services exist in the ms container or ours.
+        //      The Required needs to fail at some point to resolve an incorrect service, and this way we are not making multiple try catch blocks.
         => msServiceProvider.GetService<TService>() ?? base.GetRequiredService<TService>();
 
     protected override IScopedProvider NewScopeProvider(int scopeLevel) {
